6199077e780a764668bc9e2617435d6d23208191
Rithicaa <rithicaavm@telekomatrix.com>
2024-09-25 19:25:54 +0100

    hi

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+class Circle:
+    is_shape = True
+
+def __init__(self, radius, color='red'):
+    self.radius = radius
+    self.color = color
+
+# mycircle = Circle(23)
+# mycircle.color
+print("hello world!")
